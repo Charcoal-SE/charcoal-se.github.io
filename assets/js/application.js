2e9ad4efb2ade4bd33bfd41f4f28ed6776da44d3
Jed Fox <git@twopointzero.us>
2017-02-24 09:27:50 -0500

    InstantClick -> Turbolinks

--- assets/js/application.js
+++ assets/js/application.js
@@ -7,26 +7,21 @@ $(document).ready(function() {
     }
   }
 
-  $(document).on('click', 'a', function () {
-    if (this.hash === $(this).attr('href')) {
-      location.hash = this.hash
-      handleHash();
-      return;
-    }
-    if (
-      $(this).parents('[data-instant]').length
-   || !$(this).parents('[data-no-instant]').length) {
-      NProgress.start();
-    }
-  })
-  InstantClick.on('change', function() {
+  var navTimeout = null
+  NProgress.configure({ parent: 'html' });
+  $(document).on('turbolinks:load', function() {
+    handleHash()
     $('h1,h2,h3,h4,h5,h6').filter('[id]').filter(function () {
       return !$(this).find('.small').length
     }).each(function () {
       $(this).append(' ').append($('<span class="small">').append($('<a />').attr('href', '#' + this.id).append($('<i class="fa fa-link" />'))))
     })
-    setTimeout(NProgress.done)
+  }).on('turbolinks:request-start', function () {
+    clearTimeout(navTimeout)
+    NProgress.start()
+    // navTimeout = setTimeout(NProgress.start, 100)
+  }).on('turbolinks:request-end', function () {
+    clearTimeout(navTimeout)
+    NProgress.done()
   });
-  InstantClick.init();
-
 });
