eaad2d324a8f906226d4f648faed62cea0e29820
Jed Fox <git@twopointzero.us>
2017-02-26 07:47:31 -0500

    Fix conditional

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 git config --global user.email "git@twopointzero.us"
 git config --global user.name "Charcoal Site Bot"
 echo $TRAVIS_COMMIT
-if [ ! -n $TRAVIS_COMMIT ]
+if [ -n $TRAVIS_COMMIT ]
 then
   git commit --allow-empty -qm "Update site for $TRAVIS_COMMIT"
   git push -q
