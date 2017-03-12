74055765ab7655bc05d2f200ee7a6180a2135280
J F <git@twopointzero.us>
2017-03-12 14:01:22 -0400

    Use Smokey’s account to generate site

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
-git config --global user.email "git@twopointzero.us"
-git config --global user.name "Charcoal Site Bot"
+git config --global user.email "smokey@erwaysoftware.com"
+git config --global user.name "SmokeDetector"
 if [ -n $TRAVIS_COMMIT ]
 then
   git commit --allow-empty -qm "Update site for $TRAVIS_COMMIT"
