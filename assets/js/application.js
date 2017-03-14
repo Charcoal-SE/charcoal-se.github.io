de2ba37d93046fe79fb62077c879925c1213a958
Jed Fox <git@twopointzero.us>
2017-03-14 06:23:05 -0400

    Make the search inline

--- assets/js/application.js
+++ assets/js/application.js
@@ -14,6 +14,68 @@ $(document).ready(function() {
       $(this).append(' ').append($('<span class="small">').append($('<a />').attr('href', '#' + this.id).append($('<i class="fa fa-link" />'))))
     })
     $('.footer-insert').remove().children().prependTo($('.body + .footer'))
+
+    $('.search-link').click(function (e) {
+      openSearch()
+      e.preventDefault()
+    })
+    $('.search .close').click(function (e) {
+      e.preventDefault()
+    })
+    $('.search').click(function (e) {
+      if (this === e.target || $(e.target).hasClass('close')) {
+        closeSearch()
+      }
+    })
+
+    var index = lunr(function () {
+      this.field('id');
+      this.field('title', { boost: 10 });
+      this.field('content');
+    });
+    for (var key in window.store) {
+      if ({}.hasOwnProperty.call(window.store, key)) {
+        index.add({
+          'id': key,
+          'title': window.store[key].title,
+          'content': window.store[key].content
+        });
+      }
+    }
+
+    var term = (location.search.substring(0, 3) === '?q=') && decodeURIComponent(location.search.substring(3).replace(/\+/g, '%20'))
+    if (term) {
+      $('.search-query').val(term)
+      openSearch()
+      search(term)
+    }
+    $('.search-query').on('keyup', function (e) {
+      if (e.which === 27) {
+        closeSearch()
+      }
+      search(this.value)
+    })
+
+    function search (term) {
+      if (!term.length) {
+        $('.search-results').empty()
+        return
+      }
+      var results = index.search(term)
+      $('.search-results').empty().append(results.length ? results.map(function (res) {
+        var info = window.store[res.ref]
+        return $('<li/>').append($('<p/>').append($('<a/>').attr('href', info.url).text(info.title)))
+      }) : $('<li/>').append($('<em/>').text('No results')))
+      $('.search .link').attr('href', '/search?q=' + encodeURIComponent(term).replace(/%20/g, '+'))
+    }
+
+    function openSearch () {
+      $('.search').fadeIn('fast')
+      $('.search-query').focus()
+    }
+    function closeSearch () {
+      $('.search').fadeOut('fast')
+    }
   }).on('turbolinks:request-start', function () {
     clearTimeout(navTimeout)
     NProgress.start()
