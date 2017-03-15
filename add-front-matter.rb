25e5ffecbc7d82032da7dacc662b298d04124651
Jed Fox <git@twopointzero.us>
2017-03-15 08:36:46 -0400

    Fix index page titles

--- add-front-matter.rb
+++ add-front-matter.rb
@@ -6,4 +6,4 @@ repo_names = {
 ARGV.reject{ |path| path.include?"#{File::SEPARATOR}_" }
     .map{ |path| [path, File.new(path,"r").read] }
     .each{ |path,c|
-      File.new(path,"w").write"---\nlayout: wiki\n" + (path.include?("index.md") ? "redirect_from:\n- /#{File.dirname path}/Home.html\n- /#{File.dirname path}/Home\n" : "") + "repo_name: #{repo_names[File.basename File.dirname path]}\n---\n\n"+c }
+      File.new(path,"w").write"---\nlayout: wiki\n" + (path.include?("index.md") ? "redirect_from:\n- /#{File.dirname path}/Home.html\n- /#{File.dirname path}/Home\ntitle: Home\n" : "") + "repo_name: #{repo_names[File.basename File.dirname path]}\n---\n\n"+c }
