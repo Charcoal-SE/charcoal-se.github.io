d9e1f1b3f5bd2738d11bcf58e9a56bb78d421265
Jed Fox <git@twopointzero.us>
2017-04-16 08:51:38 -0400

    Restore user.name and user.email after pushing

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
+OLD_EMAIL=$(git config user.email)
+OLD_NAME=$(git config user.name)
 git config user.email "smokey@erwaysoftware.com"
 git config user.name "SmokeDetector"
 if [ -n $TRAVIS_COMMIT ]
@@ -8,3 +10,5 @@ else
   git commit --allow-empty -qm "Update site for <unknown commit>"
   echo -e "Skipped upload."
 fi
+git config user.email $OLD_EMAIL
+git config user.name $OLD_NAME
