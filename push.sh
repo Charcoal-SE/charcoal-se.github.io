b929cc66e198c67bac8af9069672d7a52937176e
iBug <7273074+iBug@users.noreply.github.com>
2018-09-09 13:28:07 +0800

    Override commit author with --author

--- push.sh
+++ push.sh
@@ -9,18 +9,17 @@ while [ $# -ne 0 ]; do
   esac
 done
 
-OLD_EMAIL=$(git config user.email)
-OLD_NAME=$(git config user.name)
-git config user.email "smokey@erwaysoftware.com"
-git config user.name "SmokeDetector"
+EMAIL="smokey@erwaysoftware.com"
+AUTHOR="SmokeDetector"
+
 if [ -n "$TRAVIS_COMMIT" ]; then
-  git commit --allow-empty -qm "Auto deploy from Travis CI build ${TRAVIS_BUILD_NUMBER:-<unknown>}" \
+  git commit --allow-empty -q --author "$AUTHOR <$EMAIL>"\
+    -m "Auto deploy from Travis CI build ${TRAVIS_BUILD_NUMBER:-<unknown>}" \
     -m "${COMMIT_MSG:-$TRAVIS_COMMIT}"
   git push -q
 else
-  git commit --allow-empty -qm "Auto deploy from Travis CI build ${TRAVIS_BUILD_NUMBER:-<unknown>}" \
+  git commit --allow-empty -q --author "$AUTHOR <$EMAIL>"\
+    -m "Auto deploy from Travis CI build ${TRAVIS_BUILD_NUMBER:-<unknown>}" \
     -m "${COMMIT_MSG:-$TRAVIS_COMMIT}"
   echo -e "Skipped upload."
 fi
-git config user.email $OLD_EMAIL
-git config user.name $OLD_NAME
