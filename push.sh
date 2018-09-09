6cf39fdb60219b9c8a4d36b94ae0f83c0f63ab95
iBug ♦ <7273074+iBug@users.noreply.github.com>
2018-09-09 12:10:38 +0800

    Update commit message
    Imported from https://github.com/iBug/iBug.github.io/blob/master/.travis/deploy.sh

--- push.sh
+++ push.sh
@@ -1,13 +1,25 @@
+#!/bin/bash
+
+while [ $# -ne 0 ]; do
+  case "$1" in
+    "-m"|"--message")
+      COMMIT_MSG="$2"
+      shift 2;;
+    *) break;;
+  esac
+done
+
 OLD_EMAIL=$(git config user.email)
 OLD_NAME=$(git config user.name)
 git config user.email "smokey@erwaysoftware.com"
 git config user.name "SmokeDetector"
-if [ -n $TRAVIS_COMMIT ]
-then
-  git commit --allow-empty -qm "Update site for $TRAVIS_COMMIT"
+if [ -n "$TRAVIS_COMMIT" ]; then
+  git commit --allow-empty -qm "Auto deploy from Travis CI build ${TRAVIS_BUILD_NUMBER:-<unknown>}" \
+    -m "${COMMIT_MSG:-$TRAVIS_COMMIT}"
   git push -q
 else
-  git commit --allow-empty -qm "Update site for <unknown commit>"
+  git commit --allow-empty -qm "Auto deploy from Travis CI build ${TRAVIS_BUILD_NUMBER:-<unknown>}" \
+    -m "${COMMIT_MSG:-$TRAVIS_COMMIT}"
   echo -e "Skipped upload."
 fi
 git config user.email $OLD_EMAIL
