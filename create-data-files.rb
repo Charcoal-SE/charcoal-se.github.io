c94d997397f650100f7a6c9dc42043fadbc2885c
iBug <7273074+iBug@users.noreply.github.com>
2018-09-09 19:59:59 +0800

    Drop theme commit from create-data-files.rb

--- create-data-files.rb
+++ create-data-files.rb
@@ -4,12 +4,11 @@ require 'json'
 
 smokey_commit = `git -C ./smokey rev-parse HEAD`.strip
 ms_commit = `git -C ./ms rev-parse HEAD`.strip
-theme_commit = `git -C ./.tmp rev-parse HEAD`.strip
 
 Dir.mkdir('_data') unless Dir.exist? '_data'
 
 File.open '_data/commits.json', 'w' do |f|
-  f.write(JSON.generate({ 'smokey' => smokey_commit, 'ms' => ms_commit, 'theme' => theme_commit },
+  f.write(JSON.generate({ 'smokey' => smokey_commit, 'ms' => ms_commit },
     indent: "  ",
     object_nl: "\n",
     array_nl: "\n",
